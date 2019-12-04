6d320b6da03c6240b90d050f84ddbbc8a305cef7
Johnny Sloboda <sloboda@perpetualabs.com>
2019-12-03 21:38:50 -0500

    Day 3 part 1

--- wiregrid/src/lib.rs
+++ wiregrid/src/lib.rs
@@ -1,30 +1,4 @@
 use std::collections::HashSet;
-use std::cmp::{PartialEq, Eq};
-use std::hash::Hash;
-
-pub struct Wire {
-    pts: HashSet<Point>,
-}
-
-impl Wire {
-    pub fn new(path: String) -> Wire {
-        let mut p: HashSet<Point> = HashSet::new();
-        let directions = path.split(','); //.map(
-            //|x| x.parse::<i32>().unwrap()).collect();
-        for d in directions {
-            let n = d[1..].parse::<i32>().unwrap(); 
-            let get_next_pt = match d[0] {
-                'U' => |&pt| pt.newUp(),
-                'D' => |&pt| pt.newDown(),
-                'L' => |&pt| pt.newLeft(),
-                'R' => |&pt| pt.newRight(),
-                _ => panic!("unsound input"),
-            }
-        }
-        let w = Wire{ pts: p };
-        return w;
-    }
-}
 
 #[derive(PartialEq, Eq, Hash)]
 struct Point {
@@ -32,33 +6,54 @@ struct Point {
     y: i32,
 }
 
-impl Point {
-    pub fn new(x: i32, y: i32) -> Point {
-        Point {x: x, y: y}
-    }
+pub struct Wire {
+    pts: HashSet<Point>,
+}
 
-    fn newUp(&self) -> Point {
-        Self::new(self.x, self.y+1)
-    }
-    fn newDown(&self) -> Point {
-        Self::new(self.x, self.y-1)
-    }
-    fn newLeft(&self) -> Point {
-        Self::new(self.x-1, self.y)
-    }
-    fn newRight(&self) -> Point {
-        Self::new(self.x+1, self.y)
+impl Wire {
+    pub fn new(path: &str) -> Wire {
+        let mut p: HashSet<Point> = HashSet::new();
+        let directions = path.split(',');
+        let (mut x, mut y) = (0, 0);
+        for d in directions {
+            let dir = &d[0..1];
+            let n = d[1..].trim().parse::<i32>().expect("bad input"); 
+            for _ in 0..n {
+                match dir {
+                    "U" => y += 1,
+                    "D" => y -= 1,
+                    "L" => x -= 1,
+                    "R" => x += 1,
+                    _ => panic!("unsound input"),
+                }
+                p.insert(Point{ x: x, y: y });
+            }
+        }
+        Wire{ pts: p }
     }
 }
 
-pub fn blah() {
-    println!("borto");
+pub fn closest_intersection(w1: &Wire, w2: &Wire) -> i32 {
+    w1.pts.intersection(&w2.pts)
+        .map(|pt| pt.x.abs() + pt.y.abs())
+        .min()
+        .expect("wires don't cross")
 }
 
 #[cfg(test)]
 mod tests {
+    use super::{Wire, closest_intersection};
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn given_examples_part_one() {
+        assert_eq!(6, closest_intersection(
+            &Wire::new("R8,U5,L5,D3"),
+            &Wire::new("U7,R6,D4,L4")));
+        assert_eq!(159, closest_intersection(
+            &Wire::new("R75,D30,R83,U83,L12,D49,R71,U7,L72"),
+            &Wire::new("U62,R66,U55,R34,D71,R55,D58,R83")));
+        assert_eq!(135, closest_intersection(
+            &Wire::new("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
+            &Wire::new("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")));
     }
 }
--- wiregrid/src/main.rs
+++ wiregrid/src/main.rs
@@ -1,7 +1,22 @@
 extern crate wiregrid;
 
-use wiregrid::blah;
+use std::fs::File;
+use std::io;
+use std::io::{BufReader, BufRead};
 
-fn main() {
-    blah();
+use wiregrid::{Wire, closest_intersection};
+
+fn main() -> io::Result<()> {
+    let file = File::open("input.txt")?;
+    let mut reader = BufReader::new(file);
+
+    let (mut ws1, mut ws2) = (String::new(), String::new());
+    reader.read_line(&mut ws1)?;
+    reader.read_line(&mut ws2)?;
+
+    let (w1, w2) = (Wire::new(&ws1), Wire::new(&ws2));
+    println!("Distance of closest intersection: {}",
+        closest_intersection(&w1, &w2));
+
+    Ok(())
 }
