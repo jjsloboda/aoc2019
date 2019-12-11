6a3b4690fac7f914402e171a390d50f59ef4f1c1
Johnny Sloboda <sloboda@perpetualabs.com>
2019-12-10 22:26:36 -0500

    Day 8 part 2

--- spaceimg/src/lib.rs
+++ spaceimg/src/lib.rs
@@ -1,26 +1,58 @@
-
+use std::char;
 
 pub struct Image {
     buf: Vec<u32>,
     w: u32,
     h: u32,
+    num_pixels: u32,
+    num_layers: u32,
 }
 impl Image {
     pub fn new(buf: Vec<u32>, w: u32, h: u32) -> Image {
-        Image{ buf: buf, w: w, h: h }
+        let num_px = w * h;
+        let num_layers = buf.len() as u32 / num_px;
+        Image{
+            buf: buf,
+            w: w,
+            h: h,
+            num_pixels: num_px,
+            num_layers: num_layers,
+        }
     }
     pub fn digit_count_per_layer(&self) -> Vec<[u32; 10]> {
-        let num_pixels = self.w * self.h;
-        let num_layers = self.buf.len() as u32 / num_pixels;
         let mut output = Vec::new();
-        for i in 0..num_layers {
+        for i in 0..self.num_layers {
             let mut arr: [u32; 10] = [0; 10];
-            for j in 0..num_pixels {
-                arr[self.buf[(i*num_pixels+j) as usize] as usize] += 1;
+            for j in 0..self.num_pixels {
+                arr[self.buf[(i*self.num_pixels+j) as usize] as usize] += 1;
             }
             output.push(arr);
         }
         output
+    }
+    pub fn render(&self) -> Vec<u32> {
+        let mut output = Vec::new();
+        for i in 0..self.num_pixels {
+            for l in 0..self.num_layers {
+                let px = self.buf[(l*self.num_pixels+i) as usize];
+                if px != 2 {
+                    output.push(px);
+                    break;
+                }
+            }
+        }
+        output
+    }
+    pub fn print(&self) {
+        let img = self.render();
+        for j in 0..self.h {
+            for i in 0..self.w {
+                let px = img[(j*self.w+i) as usize];
+                let c = if px == 0 { ' ' } else { char::from_digit(px, 10).unwrap() };
+                print!("{}", c);
+            }
+            println!("");
+        }
     }
 }
 
@@ -36,5 +68,13 @@ mod tests {
         assert_eq!([2,1,1,0,0,0,0,0,0,0], dcpl[0]);
         assert_eq!([1,2,1,0,0,0,0,0,0,0], dcpl[1]);
         assert_eq!([1,1,2,0,0,0,0,0,0,0], dcpl[2]);
+    }
+
+    #[test]
+    fn render_example() {
+        let img = Image::new(vec![0,2,2,2,1,1,2,2,2,2,1,2,0,0,0,0], 2, 2);
+        let rendered_img = img.render();
+        assert_eq!(4, rendered_img.len());
+        assert_eq!(vec![0,1,1,0], rendered_img);
     }
 }
--- spaceimg/src/main.rs
+++ spaceimg/src/main.rs
@@ -9,11 +9,13 @@ fn main() {
     let buf: Vec<u32> = input.trim().chars()
         .map(|x| x.to_digit(10).expect("failed to parse input"))
         .collect();
-
     let img = Image::new(buf, 25, 6);
+
     let dcpl = img.digit_count_per_layer();
     let min_zeros = dcpl.iter()
         .min_by(|x, y| x[0].cmp(&y[0]))
         .expect("no items");
     println!("one digit count * two digit count: {}", min_zeros[1] * min_zeros[2]);
+    println!("rendered image:");
+    img.print();
 }
