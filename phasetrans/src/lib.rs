1747510288229ec12665cd0f688b76f185b8a8e2
John Sloboda <sloboda@gmail.com>
2020-01-20 18:26:41 -0500

    day 16 part 2 fuck yeah!

--- phasetrans/src/lib.rs
+++ phasetrans/src/lib.rs
@@ -1,4 +1,4 @@
-
+use std::cmp::{max, min};
 
 struct PhaseRowPattern {
     i: usize,
@@ -122,6 +122,90 @@ pub fn calculate_phases_2(n: i32, a: &Vec<i32>) -> Vec<i32> {
     phase
 }
 
+enum Sign {
+    POS,
+    NEG,
+}
+
+struct BandRangeSumIter<'a> {
+    a: &'a Vec<i64>,
+    band: usize,
+    sign: Sign,
+    cur_row: usize,
+    cur_sum: i64,
+}
+impl<'a> BandRangeSumIter<'a> {
+    pub fn new(a: &'a Vec<i64>, band: usize) -> Self {
+        BandRangeSumIter{
+            a: a,
+            band: band,
+            sign: if band % 4 == 1 { Sign::POS } else { Sign::NEG },
+            cur_row: 1,
+            cur_sum: 0,
+        }
+    }
+    fn index_range(&self, row: usize) -> (usize, usize) {
+        let start = self.band * row;
+        (min(start - 1, self.a.len()), min(start + row - 1, self.a.len()))
+    }
+    fn signed_sum(&self) -> i64 {
+        match self.sign {
+            Sign::POS => self.cur_sum,
+            Sign::NEG => -self.cur_sum,
+        }
+    }
+}
+impl<'a> Iterator for BandRangeSumIter<'a> {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let (start, end) = self.index_range(self.cur_row);
+        if self.cur_row == 1 {
+            self.cur_sum += self.a[self.band - 1];
+            self.cur_row += 1;
+            Some(self.signed_sum())
+        } else if start < self.a.len() {
+            let (prev_start, prev_end) = self.index_range(self.cur_row - 1);
+            for i in prev_start..min(prev_end, start) {
+                self.cur_sum -= self.a[i];
+            }
+            for i in max(prev_end, start)..end {
+                self.cur_sum += self.a[i];
+            }
+            self.cur_row += 1;
+            Some(self.signed_sum())
+        } else {
+            None
+        }
+    }
+}
+
+pub fn calculate_phases_3(n: i64, a: &Vec<i64>) -> Vec<i64> {
+    let mut phase = a.clone();
+    for i in 0..n {
+        println!("calculating phase {}...", i+1);
+        let mut bands = Vec::new();
+        for i in 0..phase.len() {
+            if i % 2 == 0 {
+                bands.push(BandRangeSumIter::new(&phase, i+1));
+            }
+        }
+        let mut next_phase = Vec::new();
+        for d in 0..phase.len() {
+            let mut d_sum = 0;
+            for b in bands.iter_mut() {
+                if let Some(x) = b.next() {
+                    d_sum += x;
+                }
+            }
+            bands.truncate(phase.len() / ((d+1)*2) + 1 as usize);
+            next_phase.push(d_sum.abs() % 10);
+        }
+        phase = next_phase;
+    }
+    phase
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -189,16 +273,20 @@ mod tests {
 
     #[test]
     fn check_calculate_phases_basic() {
-        use super::{calculate_phases, calculate_phases_2};
+        use super::{calculate_phases, calculate_phases_2, calculate_phases_3};
         assert_eq!(vec![0, 1, 0, 2, 9, 4, 9, 8],
             calculate_phases(4, &vec![1, 2, 3, 4, 5, 6, 7, 8]));
         assert_eq!(vec![0, 1, 0, 2, 9, 4, 9, 8],
             calculate_phases_2(4, &vec![1, 2, 3, 4, 5, 6, 7, 8]));
+        assert_eq!(vec![0, 1, 0, 2, 9, 4, 9, 8],
+            calculate_phases_3(4, &vec![1, 2, 3, 4, 5, 6, 7, 8]));
     }
 
     #[test]
     fn check_calculate_phases_large_inputs() {
-        use super::{calculate_phases, calculate_phases_2};
+        //use super::{calculate_phases, calculate_phases_2};
+        use super::{calculate_phases, calculate_phases_3};
+        /*
         assert_eq!(vec![2, 4, 1, 7, 6, 1, 7, 6],
             calculate_phases(100, &vec![
                 8, 0, 8, 7, 1, 2, 2, 4, 5, 8, 5, 9, 1, 4, 5, 4, 6, 6, 1, 9, 0,
@@ -209,5 +297,21 @@ mod tests {
                 8, 0, 8, 7, 1, 2, 2, 4, 5, 8, 5, 9, 1, 4, 5, 4, 6, 6, 1, 9, 0,
                 8, 3, 2, 1, 8, 6, 4, 5, 5, 9, 5,
             ])[..8].iter().map(|&x| x).collect::<Vec<_>>());
+        */
+        assert_eq!(vec![2, 4, 1, 7, 6, 1, 7, 6],
+            calculate_phases_3(100, &vec![
+                8, 0, 8, 7, 1, 2, 2, 4, 5, 8, 5, 9, 1, 4, 5, 4, 6, 6, 1, 9, 0,
+                8, 3, 2, 1, 8, 6, 4, 5, 5, 9, 5,
+            ])[..8].iter().map(|&x| x).collect::<Vec<_>>());
+    }
+
+    #[test]
+    fn check_band_range_sum_iter() {
+        use super::BandRangeSumIter;
+        let a = vec![1, 2, 3, 4, 5, 6, 7, 8];
+        assert_eq!(vec![1, 2+3, 3+4+5, 4+5+6+7, 5+6+7+8, 6+7+8, 7+8, 8],
+            BandRangeSumIter::new(&a, 1).collect::<Vec<_>>());
+        assert_eq!(vec![-3, -13],
+            BandRangeSumIter::new(&a, 3).collect::<Vec<_>>());
     }
 }
--- phasetrans/src/main.rs
+++ phasetrans/src/main.rs
@@ -1,28 +1,28 @@
 use std::{char, io};
 use std::fs::read_to_string;
 
-use phasetrans::calculate_phases_2;
+use phasetrans::calculate_phases_3;
 
 fn main() -> io::Result<()> {
     let input = read_to_string("input.txt")?;
-    let signal: Vec<i32> = input.trim().chars()
-        .map(|ch| ch.to_digit(10).expect("failed to parse input") as i32)
+    let signal: Vec<i64> = input.trim().chars()
+        .map(|ch| ch.to_digit(10).expect("failed to parse input") as i64)
         .collect();
 
     // Part 1
-    let post_100 = calculate_phases_2(100, &signal);
+    let post_100 = calculate_phases_3(100, &signal);
     let first_8 = post_100[..8].iter()
         .map(|d| char::from_digit(*d as u32, 10).expect("bad output"))
         .collect::<String>();
     println!("first 8 after 100 phases: {}", first_8);
 
     // Part 2
-    let mut big_signal: Vec<i32> = Vec::with_capacity(signal.len() * 10_000);
+    let mut big_signal: Vec<i64> = Vec::with_capacity(signal.len() * 10_000);
     for _ in 0..10_000 {
         big_signal.extend(signal.iter());
     }
-    let offset = signal[..7].iter().fold(0, |acc, x| acc * 10 + x) as usize;
-    let big_post_100 = calculate_phases_2(1, &big_signal);
+    let offset = big_signal[..7].iter().fold(0, |acc, x| acc * 10 + x) as usize;
+    let big_post_100 = calculate_phases_3(100, &big_signal);
     let big_8 = big_post_100[offset..offset+8].iter()
         .map(|d| char::from_digit(*d as u32, 10).expect("bad output"))
         .collect::<String>();
