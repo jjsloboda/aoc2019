6447bbb11d9f07779d0273391e91c423573c3b49
John Sloboda <sloboda@gmail.com>
2020-02-17 15:48:31 -0500

    day 20 part 2

--- donutmaze/src/lib.rs
+++ donutmaze/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::io::BufRead;
 
 type Pt = (usize, usize);
@@ -6,21 +6,25 @@ type Pt = (usize, usize);
 struct Maze {
     grid: Vec<Vec<char>>,
     portals: HashMap<Pt, Pt>,
-    cur_loc: Pt,
-    end_loc: Pt,
+    inner_portals: HashSet<Pt>,
+    start: Pt,
+    end: Pt,
 }
 impl Maze {
     pub fn from_data<T: BufRead>(data: T) -> Self {
         let grid = Self::load_grid(data);
-        let (portals, start, end) = Self::build_portals_start_end(&grid);
+        let (portals, in_ptls, start, end) =
+            Self::build_portals_start_end(&grid);
         let mut maze = Maze{
             grid: grid,
             portals: portals,
-            cur_loc: start,
-            end_loc: end,
+            inner_portals: in_ptls,
+            start: start,
+            end: end,
         };
         maze.fill_dead_ends_optimizer();
-        maze.block_aa_label();
+        maze.block_terminal_labels();
+        print_grid(&maze.grid);
         maze
     }
     fn load_grid<T: BufRead>(data: T) -> Vec<Vec<char>> {
@@ -32,10 +36,11 @@ impl Maze {
             }
             grid.push(row);
         }
-        print_grid(&grid);
         grid
     }
-    fn build_portals_start_end(grid: &Vec<Vec<char>>) -> (HashMap<Pt, Pt>, Pt, Pt) {
+    fn build_portals_start_end(
+        grid: &Vec<Vec<char>>
+    ) -> (HashMap<Pt, Pt>, HashSet<Pt>, Pt, Pt) {
         let mut prtl_map = HashMap::new();
         for y in 0..(grid.len()-1) {
             for x in 0..(grid[0].len()-1) {
@@ -70,14 +75,23 @@ impl Maze {
         let start = prtl_map.remove("AA").expect("AA label not found")[0];
         let end = prtl_map.remove("ZZ").expect("ZZ label not found")[0];
         let mut portals = HashMap::new();
+        let mut inner_portals = HashSet::new();
         for pts in prtl_map.values() {
             if pts.len() != 2 {
                 panic!("portal with non-2 count");
             }
             portals.insert(pts[0], pts[1]);
             portals.insert(pts[1], pts[0]);
+            if pts[0].0 != 2 && pts[0].1 != 2 &&
+                    pts[0].0 != grid[0].len()-3 && pts[0].1 != grid.len()-3 {
+                inner_portals.insert(pts[0]);
+            }
+            if pts[1].0 != 2 && pts[1].1 != 2 &&
+                    pts[1].0 != grid[0].len()-3 && pts[1].1 != grid.len()-3 {
+                inner_portals.insert(pts[1]);
+            }
         }
-        (portals, start, end)
+        (portals, inner_portals, start, end)
     }
     fn fill_dead_ends_optimizer(&mut self) {
         // TODO super naive brute force
@@ -101,10 +115,13 @@ impl Maze {
             }
         }
     }
-    fn block_aa_label(&mut self) {
-        let (lx, ly) = Self::find_adjacent_point(&self.grid, &self.cur_loc, 'A')
+    fn block_terminal_labels(&mut self) {
+        let (ax, ay) = Self::find_adjacent_point(&self.grid, &self.start, 'A')
             .expect("no such maze");
-        self.grid[ly][lx] = '#';
+        let (zx, zy) = Self::find_adjacent_point(&self.grid, &self.end, 'Z')
+            .expect("no such maze");
+        self.grid[ay][ax] = '#';
+        self.grid[zy][zx] = '#';
     }
     fn find_adjacent_point(grid: &Vec<Vec<char>>, &(x, y): &Pt, c: char) -> Option<Pt> {
         if x > 0 && grid[y][x-1] == c {
@@ -119,12 +136,11 @@ impl Maze {
             None
         }
     }
-    pub fn distance(&self, src: Pt, dst: Pt) -> Option<u64> {
+    pub fn basic_distance(&self, src: Pt, dst: Pt) -> Option<u64> {
         let mut grid = self.grid.clone();
         let mut q = VecDeque::new();
         q.push_back((src, 0));
         while !q.is_empty() {
-            print_grid(&grid);
             let ((x, y), d) = q.pop_front().unwrap();
             if x == dst.0 && y == dst.1 {
                 return Some(d)
@@ -143,6 +159,35 @@ impl Maze {
         }
         None
     }
+    pub fn recursive_distance(&self, src: Pt, dst: Pt) -> Option<u64> {
+        let mut grids = Vec::new();
+        let mut q = VecDeque::new();
+        grids.push(self.grid.clone());
+        q.push_back((src, 0, 0));
+        while !q.is_empty() {
+            let ((x, y), d, lvl) = q.pop_front().unwrap();
+            if x == dst.0 && y == dst.1 && lvl == 0 {
+                return Some(d)
+            } else if grids[lvl][y][x].is_ascii_uppercase() {
+                let adj_pt = Self::find_adjacent_point(&grids[lvl], &(x, y), '#')
+                    .expect("no adjacent point");
+                grids[lvl][adj_pt.1][adj_pt.0] = '#';
+                if self.inner_portals.contains(&adj_pt) {
+                    grids.push(self.grid.clone());
+                    q.push_front((self.portals[&adj_pt], d, lvl+1));
+                } else if lvl > 0 {
+                    q.push_front((self.portals[&adj_pt], d, lvl-1));
+                }
+            } else if grids[lvl][y][x] == '.' {
+                grids[lvl][y][x] = '#';
+                q.push_back(((x-1, y), d+1, lvl));
+                q.push_back(((x+1, y), d+1, lvl));
+                q.push_back(((x, y-1), d+1, lvl));
+                q.push_back(((x, y+1), d+1, lvl));
+            }
+        }
+        None
+    }
 }
 
 fn print_grid(grid: &Vec<Vec<char>>) {
@@ -157,7 +202,12 @@ fn print_grid(grid: &Vec<Vec<char>>) {
 
 pub fn find_shortest_path<T: BufRead>(data: T) -> Option<u64> {
     let maze = Maze::from_data(data);
-    maze.distance(maze.cur_loc, maze.end_loc)
+    maze.basic_distance(maze.start, maze.end)
+}
+
+pub fn find_shortest_recursive_path<T: BufRead>(data: T) -> Option<u64> {
+    let maze = Maze::from_data(data);
+    maze.recursive_distance(maze.start, maze.end)
 }
 
 #[cfg(test)]
@@ -183,9 +233,54 @@ FG..#########.....#
              Z       
              Z       \n" as &[u8];
 
+    const PART2EXAMPLE: &[u8] =
+b"             Z L X W       C                 
+             Z P Q B       K                 
+  ###########.#.#.#.#######.###############  
+  #...#.......#.#.......#.#.......#.#.#...#  
+  ###.#.#.#.#.#.#.#.###.#.#.#######.#.#.###  
+  #.#...#.#.#...#.#.#...#...#...#.#.......#  
+  #.###.#######.###.###.#.###.###.#.#######  
+  #...#.......#.#...#...#.............#...#  
+  #.#########.#######.#.#######.#######.###  
+  #...#.#    F       R I       Z    #.#.#.#  
+  #.###.#    D       E C       H    #.#.#.#  
+  #.#...#                           #...#.#  
+  #.###.#                           #.###.#  
+  #.#....OA                       WB..#.#..ZH
+  #.###.#                           #.#.#.#  
+CJ......#                           #.....#  
+  #######                           #######  
+  #.#....CK                         #......IC
+  #.###.#                           #.###.#  
+  #.....#                           #...#.#  
+  ###.###                           #.#.#.#  
+XF....#.#                         RF..#.#.#  
+  #####.#                           #######  
+  #......CJ                       NM..#...#  
+  ###.#.#                           #.###.#  
+RE....#.#                           #......RF
+  ###.###        X   X       L      #.#.#.#  
+  #.....#        F   Q       P      #.#.#.#  
+  ###.###########.###.#######.#########.###  
+  #.....#...#.....#.......#...#.....#.#...#  
+  #####.#.###.#######.#######.###.###.#.#.#  
+  #.......#.......#.#.#.#.#...#...#...#.#.#  
+  #####.###.#####.#.#.#.#.###.###.#.###.###  
+  #.......#.....#.#...#...............#...#  
+  #############.#.#.###.###################  
+               A O F   N                     
+               A A D   M                     \n" as &[u8];
+
     #[test]
     fn check_example_1() {
         use super::find_shortest_path;
         assert_eq!(Some(23), find_shortest_path(EXAMPLE1));
+    }
+
+    #[test]
+    fn check_part_2_example() {
+        use super::find_shortest_recursive_path;
+        assert_eq!(Some(396), find_shortest_recursive_path(PART2EXAMPLE));
     }
 }
--- donutmaze/src/main.rs
+++ donutmaze/src/main.rs
@@ -2,13 +2,21 @@ use std::fs::File;
 use std::io;
 use std::io::BufReader;
 
-use donutmaze::find_shortest_path;
+use donutmaze::{find_shortest_path, find_shortest_recursive_path};
 
 fn main() -> io::Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
+
+    // Part 1
     let distance = find_shortest_path(reader);
     println!("shortest path: {}", distance.expect("no path found"));
+
+    // Part 2
+    let file2 = File::open("input.txt")?;
+    let reader2 = BufReader::new(file2);
+    let recursive_distance = find_shortest_recursive_path(reader2);
+    println!("shortest recursive path: {}", recursive_distance.expect("no path found"));
 
     Ok(())
 }
