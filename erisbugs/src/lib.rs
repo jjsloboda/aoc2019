bbbcdfd0a7c3b8b120e477374c7db1c7521dea2f
John Sloboda <sloboda@gmail.com>
2020-03-03 22:39:07 -0500

    day 24 part 1

--- erisbugs/src/lib.rs
+++ erisbugs/src/lib.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::collections::HashSet;
 use std::io::BufRead;
 
 type Grid = [[bool; 5]; 5];
@@ -8,7 +8,7 @@ pub fn load_grid<T: BufRead>(data: T) -> Grid {
     for (i, line) in data.lines().enumerate() {
         for (j, ch) in line.expect("error reading line from file")
                 .trim().chars().enumerate() {
-            arr[j][i] = ch == '#';
+            arr[i][j] = ch == '#';
         }
     }
     arr
@@ -17,17 +17,130 @@ pub fn load_grid<T: BufRead>(data: T) -> Grid {
 pub fn print_grid(grid: &Grid) {
     for i in 0..grid.len() {
         for j in 0..grid[0].len() {
-            let ch = if grid[j][i] { '#' } else { '.' };
+            let ch = if grid[i][j] { '#' } else { '.' };
             print!("{}", ch);
         }
         println!("");
     }
 }
 
+fn iterate_grid(grid: &Grid) -> Grid {
+    let mut new_grid = grid.clone();
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            let mut adj_bugs = 0;
+            if j > 0 && grid[j-1][i] {
+                adj_bugs += 1;
+            }
+            if j < grid[0].len()-1 && grid[j+1][i] {
+                adj_bugs += 1;
+            }
+            if i > 0 && grid[j][i-1] {
+                adj_bugs += 1;
+            }
+            if i < grid.len()-1 && grid[j][i+1] {
+                adj_bugs += 1;
+            }
+
+            if grid[j][i] && adj_bugs != 1 {
+                new_grid[j][i] = false;
+            } else if !grid[j][i] && (adj_bugs == 1 || adj_bugs == 2) {
+                new_grid[j][i] = true;
+            }
+        }
+    }
+    new_grid
+}
+
+fn biodiversity(grid: &Grid) -> u64 {
+    let mut b = 0;
+    for i in (0..grid.len()).rev() {
+        for j in (0..grid[0].len()).rev() {
+            b <<= 1;
+            if grid[i][j] {
+                b |= 1;
+            }
+        }
+    }
+    b
+}
+
+pub fn find_first_repeated(grid: &Grid) -> u64 {
+    let mut seen = HashSet::new();
+    let mut g = grid.clone();
+    loop {
+        let b = biodiversity(&g);
+        if seen.contains(&b) {
+            return b;
+        }
+        seen.insert(b);
+        g = iterate_grid(&g);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn check_read_from_file() {
+        use super::load_grid;
+        const BUF: &[u8] =
+          b"....#\n\
+            #..#.\n\
+            #..##\n\
+            ..#..\n\
+            #...." as &[u8];
+        assert_eq!(load_grid(BUF), [
+            [false, false, false, false,  true],
+            [ true, false, false,  true, false],
+            [ true, false, false,  true,  true],
+            [false, false,  true, false, false],
+            [ true, false, false, false, false],
+        ]);
+    }
+
+    #[test]
+    fn iterate_successfully() {
+        use super::iterate_grid;
+        let grid = [
+            [false, false, false, false,  true],
+            [ true, false, false,  true, false],
+            [ true, false, false,  true,  true],
+            [false, false,  true, false, false],
+            [ true, false, false, false, false],
+        ];
+        let grid1 = iterate_grid(&grid);
+        assert_eq!(grid1, [
+            [ true, false, false,  true, false],
+            [ true,  true,  true,  true, false],
+            [ true,  true,  true, false,  true],
+            [ true,  true, false,  true,  true],
+            [false,  true,  true, false, false],
+        ]);
+    }
+
+    #[test]
+    fn check_biodiversity() {
+        use super::biodiversity;
+        let grid = [
+            [false, false, false, false, false],
+            [false, false, false, false, false],
+            [false, false, false, false, false],
+            [ true, false, false, false, false],
+            [false,  true, false, false, false],
+        ];
+        assert_eq!(2129920, biodiversity(&grid));
+    }
+
+    #[test]
+    fn check_first_repeated() {
+        use super::find_first_repeated;
+        let grid = [
+            [false, false, false, false,  true],
+            [ true, false, false,  true, false],
+            [ true, false, false,  true,  true],
+            [false, false,  true, false, false],
+            [ true, false, false, false, false],
+        ];
+        assert_eq!(2129920, find_first_repeated(&grid));
     }
 }
--- erisbugs/src/main.rs
+++ erisbugs/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io;
 use std::io::BufReader;
 
-use erisbugs::{load_grid, print_grid};
+use erisbugs::{load_grid, find_first_repeated};
 
 fn main() -> io::Result<()> {
     let file = File::open("input.txt")?;
@@ -10,7 +10,10 @@ fn main() -> io::Result<()> {
     let grid = load_grid(reader);
 
     // Part 1
-    print_grid(&grid);
+    let first_rep = find_first_repeated(&grid);
+    println!("first repeated pattern: {}", first_rep);
+
+    // Part 2
 
     Ok(())
 }
